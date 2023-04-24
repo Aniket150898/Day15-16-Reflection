421cf4dca20804a440c337e3828a7ddffbb5488b
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-04-24 21:14:49 +0530

    [Aniket]- Performs Reflection Operations in C#

---
+++ Day15_Day16_ReflationPracticeProblem/ClosestNumber.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_Day16_ReflationPracticeProblem
+{
+    public class ClosestNumber
+    {
+        public string Name { get; set; }
+        public ClosestNumber()
+        {
+            Console.WriteLine("Default Constructor Invoke");
+        }
+
+        public ClosestNumber(string name)
+        {
+            Name = name;
+            Console.WriteLine("In parameterized Constructor and given Parameter is " + Name);
+        }
+
+
+        public static int FindClosestEvenNumber(int givenNumber)
+        {
+            if (givenNumber > 0)
+            {
+                int i = 0;
+                if (givenNumber == 0)
+                {
+                    return 2;
+                }
+
+                for (i = givenNumber + 1; i < givenNumber + 100; i++)
+                {
+                    int[] digitArray = ClosestNumber.getDigit(i);
+                    if (i % 2 == 0)
+                    {
+                        bool allOk = true;
+                        for (int j = 0; j < digitArray.Length; j++)
+                        {
+                            if (digitArray[j] % 2 != 0)
+                            {
+                                allOk = false;
+                                break;
+                            }
+                        }
+                        if (allOk)
+                        {
+                            return i;
+                        }
+                    }
+                }
+                return 0;
+            }
+            else 
+            {
+                for (int i = givenNumber + 1; i < givenNumber + 100; i++)
+                {
+                    i = Math.Abs(i); // for remove -ve sing
+                    int[] digitArray = ClosestNumber.getDigit(i);
+                    if (i % 2 == 0)
+                    {
+                        bool allOk = true;
+                        for (int j = 0; j < digitArray.Length; j++)
+                        {
+                            if (digitArray[j] % 2 != 0)
+                            {
+                                allOk = false;
+                                break;
+                            }
+                        }
+                        
+                        if (allOk)
+                        {
+                            i *= -1;
+                            return i;
+                        }
+                    }
+                    i *= -1; // add -ve sing
+                }
+                return 0;
+            }
+        }
+        public static int[] getDigit(int number)
+        {
+            int[] arry = number.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            return arry;            
+        }
+
+        public void SampleMethod()
+        {
+            Console.WriteLine("This is Sample Method Invoke through Reflaction");
+        }
+    }
+}
---
+++ Day15_Day16_ReflationPracticeProblem/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+
+namespace Day15_Day16_ReflationPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 15 and Day 16 Practice Problem");
+            Console.WriteLine("Select correcct option for\n" +
+                "1. To find Closest Even number with all even digit\n" +
+                "2. Fetch all class members (like methods, constructors,properties) using reflection.\n" +
+                "3. Create empty object (default constructor) of above class using reflection.\n" +
+                "4. Create parameterized object using reflection.\n" +
+                "5. Invoke method using reflection.");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please Enter Digit ");
+                    int givenNumber = Convert.ToInt32(Console.ReadLine());
+                    int closestNumber = ClosestNumber.FindClosestEvenNumber(givenNumber);
+                    Console.WriteLine("Closest Even number with all even digit is " + closestNumber);
+                    break;
+
+                case 2:
+                    //Reflaction
+                    Console.WriteLine("======******======\nClass Member Data");
+                    Type type = typeof(ClosestNumber);
+                    MemberInfo[] members = type.GetMembers();
+
+                    foreach (MemberInfo member in members)
+                    {
+                        Console.WriteLine(member);
+                    }
+                    break;
+
+                case 3:
+                    //Create empty object
+                    Console.WriteLine("======******======\nCreate empty object");
+                    Type type1 = typeof(ClosestNumber);
+                    object obj = Activator.CreateInstance(type1);
+                    Console.WriteLine(obj.GetType().Name);
+                    break;
+
+                case 4:
+                    //Create parameterized object
+                    Type type2 = typeof(ClosestNumber);
+                    object argument = "This is Mahesh's Code";
+                    object parameterizedObj = Activator.CreateInstance(type2, argument);
+                    break;
+
+                case 5:
+                    //Invoke method using reflection.
+                    Type type3 = typeof(ClosestNumber);
+                    object objForMethod = Activator.CreateInstance(type3);
+                    MethodInfo methodInfo = type3.GetMethod("SampleMethod");
+                    methodInfo.Invoke(objForMethod,null);
+
+                    //This is for Static Methode
+                    Console.WriteLine("Please Enter Digit ");
+                    int inputNumber = Convert.ToInt32(Console.ReadLine());
+                    object[] argumentOfStaticMethod = { inputNumber } ;
+                    MethodInfo staticMethodInfo = type3.GetMethod("FindClosestEvenNumber"); 
+                    int result = (int)staticMethodInfo.Invoke(null, argumentOfStaticMethod);
+                    Console.WriteLine("Closest Even number with all even digit is " + result);
+                    break;
+
+                default:
+                    Console.WriteLine("Select Correct Option for operation");
+                    break;
+            }
+        }
+    }
+}
